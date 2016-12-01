e7d67722b9a4b094496dff66ab42d8c8684167b0
JIAOFENG <vip.f@live.cn>
2016-12-01 17:52:00 +0800

    submit

--- AutoRefresh/Program.cs
+++ AutoRefresh/Program.cs
@@ -3,13 +3,8 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace AutoRefresh
 {
